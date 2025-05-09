f30f442f84012b49b6b453a2c228ee9e28173604
jsuhner <suhnejoe@students.zhaw.ch>
2025-05-09 14:41:21 +0200

    Deployement prep

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+import adapter from '@sveltejs/adapter-netlify'; 
+ 
+export default { 
+    kit: { 
+        adapter: adapter({ 
+            edge: false, 
+            split: false 
+        }) 
+    } 
 };
-
-export default config;
